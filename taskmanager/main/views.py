9f2994a9c8371c299460d0e7fa842e3d83be1cf7
Alex10885 <aledor10885@gmail.com>
2021-06-17 22:24:15 +0300

    поключение формы и добавление записей в базу

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 # Create your views here. шаблоны
 
 
 def index(request):
-    tasks = Task.objects.all() #получаем объекты на главную страницу
+    tasks = Task.objects.order_by('-id') #получаем все объекты на главную страницу по id
     return render(request, "main/index.html", {'title': 'Главная страница сайта', 'tasks': tasks})
 
 
 def about(request):
     return render(request, "main/about.html")
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        # сохраняем данные
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была не верной!'
+
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, "main/create.html", context)
