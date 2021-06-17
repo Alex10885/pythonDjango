90cae6f1c88ece5fab59a96eb9599a5c4fc75f3b
Alex10885 <aledor10885@gmail.com>
2021-06-17 20:20:32 +0300

    работа с базой

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
 # Create your views here. шаблоны
 
 
 def index(request):
-    return render(request, "main/index.html")
+    tasks = Task.objects.all() #получаем объекты на главную страницу
+    return render(request, "main/index.html", {'title': 'Главная страница сайта', 'tasks': tasks})
 
 
 def about(request):
